69da7bb79f92be6a9a047f716f66c7774e85814c
María Pérez <maria_erez@hotmail.es>
2020-09-17 13:45:08 -0500

    conexion servicios con mongodb

--- index.js
+++ index.js
@@ -4,27 +4,10 @@ const app = express();
 const { config } = require('./config/index');
 
 const moviesApi= require('./routes/movies.js');
-
+//body parser
+app.use(express.json());
 moviesApi(app)
 
-/*
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
-app.get('/json', function(req, res) {
-  res.json({ hello: 'world' });
-});
-
-app.get('/reto/:year', function(req, res) {
-    const year=req.params.year
-    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
-        res.send(`el año ${year} es bisiesto`)
-    }else{
-        res.send(`el año ${year} no es bisiesto`)
-    }
-  });
-*/
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
 });
