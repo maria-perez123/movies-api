fa1696b81e496e2835bcb66cefaa7d25373c6db0
María Pérez <maria_erez@hotmail.es>
2020-09-17 16:18:35 -0500

    implementando joi

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
+const {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema
+}=require('../utils/schemas/movies');
+
+const validationHandler=require('../utils/middlewares/validationHandler');
+
 function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
@@ -23,7 +31,7 @@ function moviesApi(app) {
     }
   });
 
-  router.get('/:movieId', async function(req, res, next) {
+  router.get('/:movieId', validationHandler({movieId:movieIdSchema}, 'params'),async function(req, res, next) {
     const { movieId } = req.params;
 
     try {
@@ -38,7 +46,7 @@ function moviesApi(app) {
     }
   });
 
-  router.post('/', async function(req, res, next) {
+  router.post('/', validationHandler(createMovieSchema),async function(req, res, next) {
     const { body: movie } = req;
     try {
       const createdMovieId = await moviesService.createMovie({ movie });
@@ -52,7 +60,7 @@ function moviesApi(app) {
     }
   });
 
-  router.put('/:movieId', async function(req, res, next) {
+  router.put('/:movieId', validationHandler({movieId:movieIdSchema}, 'params'),validationHandler(updateMovieSchema),async function(req, res, next) {
     const { movieId } = req.params;
     const { body: movie } = req;
 
@@ -71,7 +79,7 @@ function moviesApi(app) {
     }
   });
 
-  router.delete('/:movieId', async function(req, res, next) {
+  router.delete('/:movieId',validationHandler({movieId:movieIdSchema}, 'params'), async function(req, res, next) {
     const { movieId } = req.params;
 
     try {
--- utils/middlewares/validationHandler.js
+++ utils/middlewares/validationHandler.js
@@ -1,7 +1,10 @@
 const boom=require('@hapi/boom');
+const joi=require('@hapi/joi');
 
-function validate() {
-    return false;
+function validate(data, schema) {
+    const {error}=joi.object(schema).validate(data);
+    //joi.validate(data, schema);
+    return error;
   }
   
   function validationHandler(schema, check = 'body') {
